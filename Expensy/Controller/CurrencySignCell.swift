c3245105d1e91cde6a197a44c638d0d02550c4e5
Surote Gaide <surote0907@gmail.com>
2020-01-20 00:04:20 +0700

    Adding Currency Picker

---
+++ Expensy/Controller/CurrencySignCell.swift
@@ -0,0 +1,15 @@
+//
+//  CurrencyCell.swift
+//  Expensy
+//
+//  Created by Surote Gaide on 19/1/20.
+//  Copyright © 2020 Surote Gaide. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyCell: UICollectionViewCell {
+    
+    @IBOutlet weak var currencySign: UIImageView!
+    
+}
